2a9b3322c64661850c7ac543665c5aed9546e4b9
BenilsonFalcao <benilson.25104708360010@faeterj-rio.edu.br>
2025-11-18 23:19:14 -0300

    Create config.php for database settings
    Add configuration file for database connection.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php
+$config = [
+    'host' => 'localhost', // Host do seu banco de dados
+    'db_name' => 'monitoramento_pacientes', // Nome do banco de dados
+    'user' => 'root', // Usuário do banco de dados
+    'password' => '' // Senha do banco de dados
+];
